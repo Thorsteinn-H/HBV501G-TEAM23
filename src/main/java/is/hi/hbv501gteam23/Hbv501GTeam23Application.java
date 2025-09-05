4dc03d8f947651da4bf1a10fc60a091eca305058
Thorsteinn-H <thorsteinnhilmir@gmail.com>
2025-09-05 17:17:36 +0000

    first commit

---
+++ src/main/java/is/hi/hbv501gteam23/Hbv501GTeam23Application.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501gteam23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hbv501GTeam23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hbv501GTeam23Application.class, args);
+    }
+
+}
