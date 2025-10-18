0b945a6c8a802027bdb4de7184520e8ea7de95e6
Klingsterina <eyglo2001@gmail.com>
2025-10-18 15:29:17 +0000

    Bætti við seed sql til að testa endapunkta

---
+++ src/main/resources/sql/seed.sql
@@ -0,0 +1,18 @@
+INSERT INTO venue (venue_id, venue_name, latitude, longitude)
+VALUES
+    (1, 'Laugardalsvöllur', 0.5345, 0.2342),
+    (2, 'Kópavogsvöllur', 0.98524, 0.32452),
+    (3, 'Þórsvöllur', 0.234452, 0.234562);
+
+INSERT INTO team (team_id, team_name, team_country, venue_id)
+    VALUES
+        (1, 'Þróttur', 'Iceland', 1),
+        (2, 'Breiðablik', 'Iceland', 2),
+        (3, 'Þór', 'Iceland', 3);
+
+INSERT INTO player (player_id, player_name, date_of_birth, player_country, player_position, goals, team_id)
+    VALUES
+        (1, 'John Snow', '2007-12-03', 'England', 'GOALKEEPER', 13, 1),
+        (2, 'Ariana Grande', '1995-08-30', 'United states of America', 'DEFENDER', 2, 2),
+        (3, 'Jón Pétur Karlsson', '2000-08-13', 'Iceland', 'MIDFIELDER', 9, 3);
+
